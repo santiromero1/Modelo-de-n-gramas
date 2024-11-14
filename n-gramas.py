2385a6a32aa64a90fceb4588737e8fc5a98ff132
santiromero1 <romerosantiago2012@gmail.com>
2024-11-14 11:58:20 -0300

    Conseguir y limpiar un corpus de textos

--- n-gramas.py
+++ n-gramas.py
@@ -1,31 +1,25 @@
-## Modelo de n-gramas ##
-
 import re
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
+
 # Descargar datos necesarios de NLTK
-nltk.download('stopwords')
 nltk.download('punkt')
 
-
 # Cargar el archivo
-with open('nombre_del_libro.txt', 'r', encoding='utf-8') as file:
+with open('Gatsby.txt', 'r', encoding='utf-8') as file:
     texto = file.read()
 
 # Limpiar el texto
-inicio_contenido = texto.find("Inicio del contenido")  # Ajusta esto según el archivo
-fin_contenido = texto.find("Fin del contenido")
+inicio_contenido = texto.find("In my younger and more vulnerable years")
+fin_contenido = texto.find("*** END OF THIS PROJECT GUTENBERG EBOOK THE GREAT GATSBY ***")
 texto = texto[inicio_contenido:fin_contenido]  # Elimina metadatos
 texto = texto.lower()  # Convierte a minúsculas
 texto = re.sub(r'[^\w\s]', '', texto)  # Elimina puntuación
 
-# Tokenización y eliminación de stopwords
+# Tokenización sin eliminación de stopwords
 tokens = word_tokenize(texto)
-stop_words = set(stopwords.words('spanish'))
-tokens = [word for word in tokens if word not in stop_words]
 
-# Juntar en texto limpio
+# Juntar en texto limpio sin stopwords
 texto_limpio = ' '.join(tokens)
 
 print(texto_limpio[:500])  # Imprime los primeros 500 caracteres del texto limpio
